c09b4afd49437c577a692573d1e7e982185678d3
espilioto <espilioto@gmail.com>
2026-01-01 19:00:54 +0200

    Add script v1

---
+++ anidbOpenHighres.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Open fullscreen
+// @namespace    http://tampermonkey.net/
+// @version      2026-01-01
+// @description  for lazy people
+// @author       SouLSLayeR
+// @match        https://yande.re/post/show/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=yande.re
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  const highres = document.querySelector('#highres');
+  const menu = document.querySelector('#main-menu');
+
+  if (!highres || !menu) return;
+  if (document.querySelector('#open-fullsize')) return;
+
+  const link = document.createElement('a');
+  link.id = 'open-fullsize';
+  link.href = highres.href;
+  link.textContent = '🖥️ Open fullsize';
+  link.target = '_blank';
+  link.rel = 'noopener';
+
+  menu.appendChild(link);
+})();
